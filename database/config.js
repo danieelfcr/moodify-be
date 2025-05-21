e7de0520215dc6b7712bd8028e94e6b2dcadcc43
danieelfcr <danielfcab2002@gmail.com>
2025-05-21 13:18:17 -0600

    AWS: ENV CONFIGURED -NOT A FEAT-

--- database/config.js
+++ database/config.js
@@ -1,10 +1,10 @@
 const {Sequelize} = require('sequelize');
 const sequelize = new Sequelize(
-    'moodify',
-    'root',
-    '',
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
-        host: '127.0.0.1',
+        host: process.env.DB_HOST,
         dialect: 'mysql',
         logging: false,
         define: {
